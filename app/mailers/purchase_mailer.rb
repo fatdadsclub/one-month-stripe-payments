b49f23603d8ef586c54ca2ba2c5f88a39db12190
Justin <williams.justinp@gmail.com>
2016-02-23 21:05:51 -0500

    Reworked views to use product information from the database

--- app/mailers/purchase_mailer.rb
+++ app/mailers/purchase_mailer.rb
@@ -4,10 +4,10 @@ class PurchaseMailer < ActionMailer::Base
 	default from: "One Month Commerce <commerce@onemonth.com>"
 
 	def purchase_receipt purchase
-	@purchase = purchase
-	
-	mail to: purchase.email, subject: "Thanks for your purchase!"
+		@purchase = purchase
+		@product = Product.find(purchase.product_id)
+		mail to: purchase.email, subject: "Thanks for your purchase!"
 	  
-	  end
+	end
 
 end
